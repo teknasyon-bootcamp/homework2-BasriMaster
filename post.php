58090ab05f8c1cf327721d83c60a29dac6f8df23
Hasan Basri <69991414+BasriMaster@users.noreply.github.com>
2021-08-25 15:01:24 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,4 +1,12 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+<!-- burda css in kütüphanesi olan bootstrapt ı kullandım. kutuları type göre boyamak için kullandım -->
+
+
 <?php
+
+
+
 
 /**
  * post.php
@@ -23,3 +31,93 @@
  * - `getPostDetails` fonksiyonu tetiklenerek ilgili içeriğin çıktısı gösterilmeli.
  */
 
+    include_once './functions.php';
+    include_once './posts.php';
+
+    // burda eğer id, title, type varsa duracak yoksa değiştirilecek
+
+    if (isset($id)):
+        $id = $id;
+    else:
+        $id = 3;
+    endif;
+
+    if (isset($title)):
+        $title = $title;
+    else:
+        $title = "Aleyna Tilki - Sır";
+
+    endif;
+
+    if (isset($type)):
+        $type = $type;
+    else:
+        $type = "urgent";
+
+    endif;
+
+    
+
+    
+    // burada tekli postu çekiyoruz
+
+    $getLatestPosts = getLatestPosts($getRandomPostCount);
+
+ ?> 
+
+
+
+    <!-- burada bootstrap yardımı ile php de bir döngü başlatarak sırayla rekrana gösterdim -->
+
+
+    <div class="container mt-5 mb-5 w-75">
+
+        <h3 class="m-5">Toplam Görüntülenen post : <?= $getRandomPostCount; ?></h3>
+
+
+        <?php foreach ($getLatestPosts as $key => $value): ?>
+
+
+        <?php 
+
+            switch ($value['type']) {
+        case 'urgent':
+            $class = 'bg-danger text-white';
+            break;
+        case 'warning':
+            $class = 'bg-warning';
+            break;
+        case 'normal':
+            $class = 'bg-light';
+            break;
+        
+    }
+
+        ?>
+
+        <div class="row mt-3">
+            <div class="card <?php echo $class; ?>">
+                <span class="card-body text-center">
+                    <?php 
+
+                        getPostDetails($key, $value['title']);
+
+                    ?>
+                </span>
+            </div>
+        </div>
+
+        <?php  endforeach; ?>
+
+
+    </div>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    
